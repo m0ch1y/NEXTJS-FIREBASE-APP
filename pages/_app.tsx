14c14bb0ba4299747626f2d100ffa9fb50811bfe
m0ch1y <Kaito.m_317@outlook.jp>
2023-03-10 21:56:32 +0900

    _appにapppropsを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import "../lib/firebase";
 import "../styles/globals.scss";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
+import { AppProps } from "next/app";
 
 dayjs.locale("ja");
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
