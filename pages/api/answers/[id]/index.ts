b43c770926e2ccf1f6e55ab5415721bb07c5835f
m0ch1y <Kaito.m_317@outlook.jp>
2023-03-12 05:55:32 +0900

    chapter16の途中

--- pages/api/answers/[id]/index.ts
+++ pages/api/answers/[id]/index.ts
@@ -1,13 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import "../../../../lib/firebase_admin";
 import { firestore } from "firebase-admin";
+import { Answer } from "./../../../../models/Answer";
+import { Question } from "./../../../../models/Question";
 
-const request = async (req: NextApiRequest, res: NextApiResponse) => {
-  const id = req.query.id as string;
-
-  const doc = await firestore().collection("answers").doc(id).get();
-
-  res.status(200).json(doc.data());
+type Data = {
+  answer: Answer;
+  question: Question;
 };
 
-export default request;
+const answerApi = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const id = req.query.id as string;
+
+  const answerDoc = await firestore().collection("answers").doc(id).get();
+  const answer = answerDoc.data() as Answer;
+  answer.id = answerDoc.id;
+
+  const questionDoc = await firestore()
+    .collection("questions")
+    .doc(answer.questionId)
+    .get();
+  const question = questionDoc.data() as Question;
+  question.id = questionDoc.id;
+
+  res.status(200).json({
+    answer,
+    question,
+  });
+};
+
+export default answerApi;
