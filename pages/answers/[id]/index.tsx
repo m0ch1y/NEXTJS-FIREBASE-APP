15d3fceaabd09c4249254c9d3c665c3e3717e9f1
m0ch1y <Kaito.m_317@outlook.jp>
2023-03-13 02:57:43 +0900

    chapter16

--- pages/answers/[id]/index.tsx
+++ pages/answers/[id]/index.tsx
@@ -1,11 +1,20 @@
 import Layout from "../../../components/Layout";
 import { Answer } from "../../../models/Answer";
 import { Question } from "../../../models/Question";
+import Head from "next/head";
 
 type Props = {
   answer: Answer;
   question: Question;
 };
+
+function getDescription(answer: Answer) {
+  const body = answer.body.trim().replace(/[ \r\n]/g, "");
+  if (body.length < 140) {
+    return body;
+  }
+  return body.substring(0, 140) + "...";
+}
 
 export async function getServerSideProps({ query }: any) {
   const res = await fetch(process.env.API_URL + `/api/answers/${query.id}`);
@@ -14,9 +23,18 @@ export async function getServerSideProps({ query }: any) {
 }
 
 export default function AnswersShow(props: Props) {
-  console.log(props);
+  const description = getDescription(props.answer);
+
   return (
     <Layout>
+      <Head>
+        <meta name="description" key="description" content={description} />
+        <meta
+          property="og:description"
+          key="ogDescription"
+          content={description}
+        />
+      </Head>
       <div className="row justify-content-center">
         <div className="col-12 col-md-6">
           <>
